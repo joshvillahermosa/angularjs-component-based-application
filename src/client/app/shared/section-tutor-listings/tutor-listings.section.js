e4711ded3f2bfce63fa11e9c78511d227b5f6a6d
Joshua J. Villahermosa <joshvillahermosa@gmail.com>
2017-02-28 17:11:59 -0500

    feat(tutor-listings-section): Created base section for tutor listings
    BREAKING CHANGE: Need tests

---
+++ src/client/app/shared/section-tutor-listings/tutor-listings.section.js
@@ -0,0 +1,27 @@
+(function () {
+  'use strict';
+
+  function TutorListingsController () {
+    var vm = this;
+
+    vm.headerType = 'h2';
+    vm.headerText = 'Checkout our boring tutors!';
+    vm.mainText = ['Lorem this Ipsum! We have so many things that we have to do ',
+    'better... and certainly ipsum is one of them. That other text? Sadly, it’s',
+    ' no longer a 10. Lorem Ipsum is unattractive, both inside and out. I fully',
+    'understand why it’s former users left it for something else. They made a',
+    'good decision.'].join('');
+
+    init();
+
+    function init() { }
+  }
+
+  var TutorListingsSection = {
+    templateUrl: 'app/shared/section-tutor-listings/tutor-listings.section.html',
+    controller: TutorListingsController
+  };
+
+  angular.module('app.shared')
+    .component('ttTutorListingsSection', TutorListingsSection);
+})();
